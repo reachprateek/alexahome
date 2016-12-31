5ec9362b1290f2d2c2daf2e124e51a4a26e0ecaa
Prateek Jain <reachprateek@gmail.com>
2016-12-30 17:45:33 -0800

    updating the transmitter code

--- transmitter.py
+++ transmitter.py
@@ -2,27 +2,25 @@ import time
 import sys
 import RPi.GPIO as GPIO
 
-#a_on = '1111111111111010101011101'
-#a_on = '0001010101010101001100110'
-#a_off = '0001010101010101001111000'
 
-#a_on = '1110101010101010110011001'
-#a_off = '1111111111111010101010111'
-#b_on = '1111111111101110101011101'
-#b_off = '1111111111101110101010111'
-#c_on = '1111111111101011101011101'
-#c_off = '1111111111101011101010111'
-#d_on = '1111111111101010111011101'
-#d_off = '1111111111101010111010111'
+#ch1_on = '0100100000110001111100110101000100010001'
+#ch1_on = '0100100000110001111100110101000100011110'
+fr_ch1_on = '0100100000110001111100110101000100011110'
+sr_ch1_up = '1101000000110001001101110010000100010001'
+sr_ch1_down = '1101000000110001001101110010000100110011'
+sr_ch1_stop = '1101000000110001001101110010000101010101'
 
-ch1_on = '0100100000110001111100110101000100010001'
-
-short_delay = 0.00050
-long_delay = 0.00085
+starter_code_delay = 0.0047
+time_to_first_bit_delay = 0.0015
+short_delay = 0.00028
+long_delay = 0.00062
+zero_to_delay = 0.0008
+one_to_delay = 0.00045
 extended_delay = 0.01
 
 NUM_ATTEMPTS = 10
 TRANSMIT_PIN = 17
+#TRANSMIT_PIN = 2
 
 def transmit_code(code):
     '''Transmit a chosen code string using the GPIO transmitter'''
@@ -30,17 +28,21 @@ def transmit_code(code):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(TRANSMIT_PIN, GPIO.OUT)
     for t in range(NUM_ATTEMPTS):
+        GPIO.output(TRANSMIT_PIN, 1)
+        time.sleep(starter_code_delay)
+        GPIO.output(TRANSMIT_PIN, 0)
+        time.sleep(time_to_first_bit_delay)
         for i in code:
             if i == '0':
                 GPIO.output(TRANSMIT_PIN, 1)
                 time.sleep(short_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(long_delay)
+                time.sleep(zero_to_delay)
             elif i == '1':
                 GPIO.output(TRANSMIT_PIN, 1)
                 time.sleep(long_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(short_delay)
+                time.sleep(one_to_delay)
             else:
                 continue
         GPIO.output(TRANSMIT_PIN, 0)
